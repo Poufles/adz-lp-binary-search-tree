2de680a484a307d9337358864a7bf1f9c22b6fb9
Evan Ayalin <siyeel04@gmail.com>
2025-05-16 16:57:29 +0800

    Added insert, delete, and find

--- binary-search.mjs
+++ binary-search.mjs
@@ -1,5 +1,12 @@
 import MergeSort from "./merge-sort.mjs";
 
+/**
+ * NOTE IN FUNCTION INSERT
+ * # THE FUNCTION DOES NOT INSERT 
+ * # THE NEW VALUE IN THE ARRAY.  
+ */
+///
+
 function Node() {
     let root, left, right;
 
@@ -12,46 +19,14 @@ function Node() {
 
 /**
  * Creates a node tree.
- * @param {Array} arr 
+ * @param {Array} arr
  */
 export default function NodeTree(arr) {
     let root;
     const sortedArr = RemoveDuplicates(MergeSort(arr));
 
-    console.log(`Original Arr: ${arr}`);
-    console.log(`Sorted Arr: ${sortedArr}`);
-
-    /**
-     * 
-     * @param {Array} arr 
-     */
-    function tree(arr, start, end) {
-        if (start > end) return null;
-
-        const midpoint = Math.ceil((start + end) / 2);
-        const root = arr[midpoint];
-        const node = Node();
-
-        node.root = root;
-        node.left = tree(arr, start, midpoint - 1);
-        node.right = tree(arr, midpoint + 1, end);
-
-        return node;
-    };
-
-    function print(node, prefix, isLeft) {
-        if (node === null) return;
-
-        if (node.right !== null) {
-            print(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-        };
-
-        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.root}`);
-
-        if (node.left !== null) {
-            print(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-        };
-
+    const prettyPrint = () => {
+        print(root, '', true);
     };
 
     const buildTree = () => {
@@ -60,17 +35,97 @@ export default function NodeTree(arr) {
         return root;
     };
 
-    const prettyPrint = () => {
-        print(root, '', true);
+    const insert = (value) => {
+        InsertNode(root, value);
     };
 
+    const deleteItem = (value) => {
+        const nodeToBeDeleted = find(value);
+
+        if (!nodeToBeDeleted) return null;
+
+        const lowestNode = FindLowestNode(nodeToBeDeleted);
+        const lowestNodeParent = FindParentNode(nodeToBeDeleted, lowestNode);
+
+        nodeToBeDeleted.root = lowestNode.root;
+
+        if (lowestNodeParent.left && lowestNodeParent.left.root === lowestNode.root) lowestNodeParent.left = null;
+        if (lowestNodeParent.right && lowestNodeParent.right.root === lowestNode.root) lowestNodeParent.right = null;
+
+        prettyPrint();
+    };
+
+    const find = (value) => {
+        return FindNode(root, value);
+    };
 
     return {
         prettyPrint,
-        buildTree
+        buildTree,
+        insert,
+        deleteItem,
+        find,
+        
     };
 };
 
+function tree(arr, start, end) {
+    if (start > end) return null;
+
+    const midpoint = Math.ceil((start + end) / 2);
+    const root = arr[midpoint];
+    const node = Node();
+
+    node.root = root;
+    node.left = tree(arr, start, midpoint - 1);
+    node.right = tree(arr, midpoint + 1, end);
+
+    return node;
+};
+
+function InsertNode(node, value) {
+    if (node === null) {
+        const newNode = Node();
+        newNode.root = value;
+        newNode.left = null;
+        newNode.right = null;
+
+        return newNode;
+    };
+
+    if (node.root > value) node.left = InsertNode(node.left, value);
+    else node.right = InsertNode(node.right, value);
+
+    return node;
+};
+
+function FindNode(node, value) {
+    if (node === null) return null;
+    if (node.root === value) return node;
+    if (node.root > value) return FindNode(node.left, value);
+    return FindNode(node.right, value);
+};
+
+function FindLowestNode(node) {
+    let left, right;
+
+    if (node.left) left = FindLowestNode(node.left);
+    if (node.right) right = FindLowestNode(node.right);
+
+    if (left && right) return left > right ? right : left;
+    if (left) return left;
+    if (right) return right;
+    
+    return node;
+};
+
+function FindParentNode(parent, node) {
+    if (parent.left && parent.left.root === node.root) return parent;
+    if (parent.right && parent.right.root === node.root) return parent;
+
+    if (parent.left) return FindParentNode(parent.left, node);
+    if (parent.right) return FindParentNode(parent.right, node);
+};
 
 function RemoveDuplicates(arr) {
     const sortedArr = [];
@@ -88,4 +143,18 @@ function RemoveDuplicates(arr) {
     };
 
     return sortedArr;
+};
+
+function print(node, prefix, isLeft) {
+    if (node === null) return;
+
+    if (node.right !== null) {
+        print(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    };
+
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.root}`);
+
+    if (node.left !== null) {
+        print(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    };
 };
--- main.mjs
+++ main.mjs
@@ -1,15 +1,23 @@
 import NodeTree from "./binary-search.mjs";
 
-const tree = NodeTree([1, 4, 4, 4]);
+const tree = NodeTree([2, 4, 5, 8, 9]);
 const x = tree.buildTree();
-console.log(x)
-console.log(`Main Root: ${x.root}`);
-console.log(`Main Left: ${x.left}`);
-console.log(`Main Right: ${x.right}`);
+// console.log(x)
+// console.log(`Main Root: ${x.root}`);
+// console.log(`Main Left: ${x.left}`);
+// console.log(`Main Right: ${x.right}`);
 // console.log(`Main Left Root: ${x.left.root}`);
 // console.log(`Main Left Left: ${x.left.left}`);
 // console.log(`Main Left Right: ${x.left.right}`);
 // console.log(`Main Right Root: ${x.right.root}`);
 // console.log(`Main Right Left: ${x.right.left}`);
 // console.log(`Main Right Right: ${x.right.right}`);
+tree.insert(3);
 tree.prettyPrint();
+console.log('\n')
+// console.log(tree.find(3));
+tree.deleteItem(5);
+// console.log({3} === {3})
+// tree.insert(10);
+// tree.insert(6);
+// tree.prettyPrint();
